f6aee27ae73287691d592e66873d56842a5530ae
vtpa <phuonganh.ct2002@gmail.com>
2026-05-27 23:12:14 +0700

    done swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,28 @@ function checkScroll() {
 }
 window.addEventListener('DOMContentLoaded', checkScroll);
 window.addEventListener('scroll', checkScroll);
+const swiper = new Swiper('.productSwiper', {
+    
+    slidesPerView: 2, 
+    spaceBetween: 20, 
+    loop: true,       
+    autoplay: {       
+             delay: 3000,  
+             disableOnInteraction: false, 
+         },
+
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {    
+             640: {
+                 slidesPerView: 2, 
+             },
+             992: {
+                 slidesPerView: 4, 
+             },
+         },
+});
+
