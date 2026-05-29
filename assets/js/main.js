8f5a719e5f6b0bec2846dd8a2408cdde0642561a
vtpa <phuonganh.ct2002@gmail.com>
2026-05-29 22:49:03 +0700

    done scroll btn + fb + zl

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,10 +37,10 @@ const swiperList = new Swiper('.productListSwiper',{
     slidesPerView: 2, 
     spaceBetween: 30, 
     loop: true,       
-    // autoplay: {       
-    //          delay: 3000,  
-    //          disableOnInteraction: false, 
-    //      },
+    autoplay: {       
+             delay: 3000,  
+             disableOnInteraction: false, 
+         },
 
     
     navigation: {
@@ -55,59 +55,51 @@ const swiperList = new Swiper('.productListSwiper',{
                  slidesPerView: 5, 
              },
          },
-    // breakpoints: {
-    //     0: {
-    //         slidesPerView: 2.2,
-    //         // Khoảng cách giữa các vòng tròn trên Mobile (15px)
-    //         spaceBetween: 15, 
-    //     },
-    //     768: {
-    //         slidesPerView: 3.5,
-    //         // Khoảng cách trên Tablet (30px)
-    //         spaceBetween: 30, 
-    //     },
-    //     1024: {
-    //         slidesPerView: 5,
-    //         // TĂNG KHOẢNG CÁCH TRÊN PC: Bạn thay đổi con số này (ví dụ: 50, 60, 80)
-    //         // Số càng lớn, các vòng tròn càng cách xa nhau
-    //         spaceBetween: 60, 
-    //     },
-    // }
 });
 const comboSwiper = new Swiper('.comboSwiper', {
-    // Không tự động loop như bạn yêu cầu
     loop: false, 
     
-    // Con trỏ bàn tay kéo thả
     grabCursor: true, 
 
-    // LOGIC MOBILE (Mặc định): 2 cột, 2 hàng = 4 thẻ
     slidesPerView: 2, 
     grid: {
         rows: 2, 
-        fill: 'row' // Đổ data theo hàng ngang trước
+        fill: 'row' 
     },
-    spaceBetween: 15, // Khoảng cách trên mobile
+    spaceBetween: 15, 
 
-    // LOGIC RESPONSIVE BẺ CONG GIAO DIỆN
     breakpoints: {
-        // Màn hình Tablet (iPad)
         768: {
             slidesPerView: 3, 
             grid: {
-                rows: 1, // Trở về 1 hàng ngang
+                rows: 1,
             },
             spaceBetween: 20,
         },
-        // Màn hình Laptop/PC (Đúng yêu cầu: 4 thẻ trên 1 hàng)
         1024: {
             slidesPerView: 4, 
             grid: {
-                rows: 1, // Khóa cứng 1 hàng ngang
+                rows: 1, 
             },
-            spaceBetween: 30, // Giãn cách rộng ra cho thoáng
+            spaceBetween: 30,
         }
     }
+});
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        scrollTopBtn.classList.add('show');
+    } else {
+        scrollTopBtn.classList.remove('show');
+    }
+});
+
+scrollTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
 
 
